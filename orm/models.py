e0743e399877964ff628a1cf661fa1cda45d4d28
Sedly <sedly.keith@student.moringaschool.com>
2024-06-13 15:10:34 +0300

    updating class authors in models

--- orm/models.py
+++ orm/models.py
@@ -12,11 +12,3 @@ class Author(Base):
 
     books = relationship("Book", back_populates="author")
 
-class Book(Base):
-    __tablename__ = 'books'
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    author_id = Column(Integer, ForeignKey('authors.id'))
-
-    author = relationship("Author", back_populates="books")
